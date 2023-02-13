0e85d107621fd94a60d7d641655d08876eb845d8
dinarfathrahmanov <dinfat@mail.ru>
2023-02-13 20:05:16 +0300

    задача 2

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,21 @@
 public class Main {
-        public static void main(String[] args) {
-            task1();
-        }
+    public static void main(String[] args) {
+        task1();
+        task2();
+    }
 
-        public static void task1 () {
-            System.out.println("Задача 1");
-        for (int i=1;i<=10;i++){
+    public static void task1() {
+        System.out.println("Задача 1");
+        for (int i = 1; i <= 10; i++) {
             System.out.println(i);
         }
+    }
+
+    public static void task2() {
+        System.out.println("Задача 2");
+        for (int i = 10; i > 0; i--) {
+            System.out.println(i);
         }
+    }
 }
+
