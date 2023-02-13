91603eeeb44a2841b46f998f3e593640a8326b77
dinarfathrahmanov <dinfat@mail.ru>
2023-02-13 20:37:47 +0300

    задача 5

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
         task2();
         task3();
         task4();
+        task5();
     }
 
     public static void task1() {
@@ -31,7 +32,15 @@ public class Main {
             System.out.println(i);
         }
     }
+    public static void task5() {
+        System.out.println("Задача 5");
+        for (int i = 1904; i <= 2096; i = i + 4) {
+            System.out.println(i+" год является високосным");
+        }
+    }
+
 }
+
 
 
 
